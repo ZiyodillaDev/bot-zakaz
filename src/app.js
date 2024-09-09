f0e875245182b850da2bd998e1758ef248bf4edd
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2024-09-09 15:01:14 +0500

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // Bot
 require("dotenv/config");
+const express = require("express");
 const { Bot } = require("grammy");
 
 const start = require("./start/run");
@@ -7,6 +8,19 @@ const all = require("./start/modules");
 
 const token = process.env.BotToken;
 const bot = new Bot(token);
+const app = express();
+
+app.post("/webhook", (req, res) => {
+  bot.handleUpdate(req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+const WebHook = process.env.WebHook;
+bot.api.setWebhook(WebHook);
 
 all(bot);
 start(bot);
